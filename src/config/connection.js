ae005006b01c5defc97269f9bc396b8e631a65a4
HanSigma <han678556@gmail.com>
2025-06-23 00:23:46 +0700

    Ealah

--- src/config/connection.js
+++ src/config/connection.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/Penelitians", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
--- src/seeder/adminSeeder.js
+++ src/seeder/adminSeeder.js
@@ -6,7 +6,7 @@ dotenv.config(); // ini juga oke kalau .env di folder yang sama dengan tempat ka
 
 const seedAdmin = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/Penelitians", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
